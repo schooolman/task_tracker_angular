b568e5e5fb253b45a46d401fdd8ccb70c90dd059
schooolman <j.schoolmee@gmail.com>
2015-10-05 10:00:40 -0500

    clean up code

--- client/app.js
+++ client/app.js
@@ -17,8 +17,6 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
                 throw new Error('Failed to get tasks');
             }
             $scope.newTaskList = response.data;
-            //console.log("this is the new task list respons: " + $scope.newTaskList[0].item);
-            //console.log("This is newTask" + $scope.newTask.item);
             return response.data;
 
         });
@@ -26,12 +24,9 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
 
     //remove completed tasks
     $scope.removeDone = function (){
-        //console.log("current array of tasks" + $scope.newTaskList);
-        //var loopLength = $scope.newTaskList.length;
         for(var i = 0; i < $scope.newTaskList.length; i++) {
             console.log($scope.newTaskList.length);
             if($scope.newTaskList[i].done === true){
-                //console.log("this is the loop: " + $scope.newTaskList[i].done);
                 $scope.newTaskList.splice(i, 1);
                 i--;
             }
@@ -44,12 +39,11 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
     $scope.add = function(){
         if($scope.freshTask == ''){
             alert("please enter task");
-        }else {
+        }else{
             var taskTemp = {};
             taskTemp.item = $scope.freshTask;
             taskTemp.done = false;
 
-            //console.log('hit add task functionality: ' + taskTemp.item);
             $scope.newTaskList.push(taskTemp);
             $scope.freshTask = '';
             console.log($scope.newTaskList);
@@ -60,29 +54,18 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
 
     //Sort completed to bottom of list
     $scope.sort = function(){
-        //console.log("clicking");
         for(var i = 0; i < $scope.newTaskList.length; i++){
             if($scope.newTaskList[i].done == true){
                 console.log($scope.newTaskList[i]);
-                //var moveItem = $scope.newTaskList[i];
                 var move = $scope.newTaskList.splice(i, 1);
                 $scope.newTaskList.push(move[0]);
                 return $scope.newTaskList;
-                //console.log($scope.newTaskList);
 
             }
         }
     };
 
 
-
-    //Remove items from task list
-    $scope.completeTask = function(array, index){
-
-        var completeItem = array.indexOf(index);
-        //$scope.newTaskList.splice(completeItem, 1);
-        $scope.changeCSS();
-    };
 
     loadTasks();
 
